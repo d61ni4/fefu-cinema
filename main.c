ef85be4560e925a98565689b10540deff3bec6b1
d61ni4 <butus4232@mail.ru>
2022-12-13 13:20:19 +1000

    Initial commit

--- main.c
+++ main.c
@@ -1,24 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct Node {
-    void *value;
-    struct Node *next;
-    struct Node *prev;
-} Node;
-
-typedef struct DblLinkedList {
-    size_t size;
-    Node *head;
-    Node *tail;
-} DblLinkedList;
-
-DblLinkedList* createDblLinkedList() {
-    DblLinkedList *tmp = (DblLinkedList*) malloc(sizeof(DblLinkedList));
-    tmp->size = 0;
-    tmp->head = tmp->tail = NULL;
-    return tmp;
-}
 
 int main() {
     printf("Hello, World!\n");
