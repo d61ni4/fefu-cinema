b4af63c43aea71e90f118daef273b9edc65a2ac7
d61ni4 <butus4232@mail.ru>
2022-12-13 14:36:54 +1000

    added functions for pushing and popping list

--- main.c
+++ main.c
@@ -39,6 +39,69 @@ void push(DblLinkedList *list, int data) {
     list->size++;
 }
 
+
+int pop(DblLinkedList *list) {
+    Node *prev;
+    int tmp;
+    if (list->head == NULL) {
+        exit(2);
+    }
+    prev = list->head;
+    list->head = list->head->next;
+    if (list->head) {
+        list->head->prev = NULL;
+    }
+    if (prev == list->tail) {
+        list->tail = NULL;
+    }
+    tmp = prev->value;
+    free(prev);
+
+    list->size--;
+    return tmp;
+}
+
+void pushBack(DblLinkedList *list, int value) {
+    Node *tmp = (Node*) malloc(sizeof(Node));
+    if (tmp == NULL) {
+        exit(3);
+    }
+    tmp->value = value;
+    tmp->next = NULL;
+    tmp->prev = list->tail;
+    if (list->tail) {
+        list->tail->next = tmp;
+    }
+    list->tail = tmp;
+
+    if (list->head == NULL) {
+        list->head = tmp;
+    }
+    list->size++;
+}
+
+int popBack(DblLinkedList *list) {
+    Node *next;
+    int tmp;
+    if (list->tail == NULL) {
+        exit(4);
+    }
+
+    next = list->tail;
+    list->tail = list->tail->prev;
+    if (list->tail) {
+        list->tail->next = NULL;
+    }
+    if (next == list->head) {
+        list->head = NULL;
+    }
+    tmp = next->value;
+    free(next);
+
+    list->size--;
+    return tmp;
+}
+
 void circleList (DblLinkedList* list) {
     list->tail->next = list->head;
     list->head->prev = list->tail;
@@ -52,6 +115,14 @@ int main() {
     circleList(list);
     printf("Size %d, data %d\n", list->size, list->head->next->value);
     printf("Size %d, data %d\n", list->size, list->head->next->next->value);
-    printf("Size %d, data %d", list->size, list->head->next->next->next->value);
+    printf("Size %d, data %d\n", list->size, list->head->next->next->next->value);
+    int n = pop(list);
+    printf("%d", n);
+    printf("Size %d, data %d\n", list->size, list->head->next->next->value);
+    pushBack(list, 7);
+    printf("Size %d, data %d\n", list->size, list->tail->value);
+    n = popBack(list);
+    printf("%d %d", n, list->size);
+
     return 0;
 }
